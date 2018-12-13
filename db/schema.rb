160e6e06bbd97f4989d3faafef786c707de6cf9a
watepu <40508321+watepu@users.noreply.github.com>
2018-12-13 19:18:47 +0900

    セキュアパスワード設定、カラム名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181213100458) do
+ActiveRecord::Schema.define(version: 20181213101540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false

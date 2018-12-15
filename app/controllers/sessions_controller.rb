b3d0f37da8d54ec4459acfb7ca3c332765cc1f77
watepu <40508321+watepu@users.noreply.github.com>
2018-12-15 13:35:26 +0900

    edit,update作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    sesson.delete(:user_id)
+    session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to tops_index_path
+    else
+      render 'edit'
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end

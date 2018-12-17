33673a6406fe96c04910dcbb1a89416a841bd023
y.okada <ecopack516@gmail.com>
2018-12-17 19:29:32 +0900

    blogs/create作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,18 @@
 class BlogsController < ApplicationController
   def new
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+     redirect_to tops_index_path
+    else
+     render 'new'
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end

48d7185f075bef6979a99181a6ee32474e08d6cc
y.okada <ecopack516@gmail.com>
2018-12-17 18:31:10 +0900

    Blogモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181216092519) do
+ActiveRecord::Schema.define(version: 20181216093109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
